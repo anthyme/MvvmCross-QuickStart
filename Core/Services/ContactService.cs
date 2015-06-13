b709c02dbf7aa2e2714d1cec8ed4b7b2de0d24ed
anthyme <anthyme@live.com>
2015-06-13 10:23:46 +0200

    add contact model/service/interfacem reference json.net nuget on core porject

---
+++ Core/Services/ContactService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Core.Models;
+using Newtonsoft.Json;
+
+namespace Core.Services
+{
+    public interface IContactService
+    {
+        IEnumerable<Contact> GetAll();
+    }
+
+    public class ContactService : IContactService
+    {
+        public IEnumerable<Contact> GetAll()
+        {
+            var serializer = new JsonSerializer();
+            using(var stream = typeof(ContactService).GetTypeInfo().Assembly
+                .GetManifestResourceStream("Core.Resources.data.json"))
+            using (var reader = new StreamReader(stream))
+            using (var jsonTextReader = new JsonTextReader(reader))
+            {
+                return serializer.Deserialize<IEnumerable<Contact>>(jsonTextReader);
+            }
+        } 
+    }
+}
